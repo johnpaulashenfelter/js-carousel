3dad83516c6548141e4034d28512cd50ae0fff4c
John Paul Ashenfelter <johnpaul@transitionpoint.com>
2019-05-07 13:22:03 -0400

    Better specs

--- stimulus-carousel/src/tests/carousel_controller.test.js
+++ stimulus-carousel/src/tests/carousel_controller.test.js
@@ -1,6 +1,6 @@
 import { Application } from "stimulus"
 import CarouselController from "carousel_controller"
-// import slides from '../slides'
+import slides from '../slides'
 
 const changeValue = (element, value, eventType) => {
   const event = new Event(eventType);
@@ -33,30 +33,42 @@ describe('CarouselController', () => {
   it('increments `index` when Next is clicked', () => {
     const carousel = document.querySelector('div')
     const btn = document.querySelector('[data-action="carousel#next"]')
-    expect(carousel.dataset.carouselIndex).toBe("0");
-    changeValue(btn, null, "click");
+    carousel.dataset.carouselIndex = 1
     expect(carousel.dataset.carouselIndex).toBe("1");
+    changeValue(btn, null, "click");
+    expect(carousel.dataset.carouselIndex).toBe("2");
   });
 
   it('decrements `index` when Prev is clicked', () => {
     const carousel = document.querySelector('div')
     const btn = document.querySelector('[data-action="carousel#previous"]')
-    const index = document.querySelector('div').dataset.carouselIndex
     carousel.dataset.carouselIndex = 3
     expect(carousel.dataset.carouselIndex).toBe("3");
     changeValue(btn, null, "click");
     expect(carousel.dataset.carouselIndex).toBe("2");
   });
   
+  describe('with the first slide selected', () => {
+    it('wraps `index` to the max value when Prev is clicked', () => {
+      const carousel = document.querySelector('div')
+      const btn = document.querySelector('[data-action="carousel#previous"]')
+      changeValue(btn, null, "click");
+      expect(carousel.dataset.carouselIndex).toBe(`${slides.length - 1}`);
+    });
+  });
+  describe('with the last slide selected', () => {
+    it('wraps `index` to the min vaue when Next is clicked', () => {
+      const carousel = document.querySelector('div')
+      const btn = document.querySelector('[data-action="carousel#next"]')
+      const index = document.querySelector('div').dataset.carouselIndex
+      carousel.dataset.carouselIndex = slides.length - 1
+      changeValue(btn, null, "click");
+      expect(carousel.dataset.carouselIndex).toBe("0");
+    });
+  });
 })
 
 
-
-// it('increments `slideIndex` when Next is clicked', () => {
-//   wrapper.setState({ slideIndex: 1 });
-//   wrapper.find('[data-action="next"]').simulate('click');
-//   expect(wrapper.state('slideIndex')).toBe(2);
-// });
 
 // it('renders the current slide as a CarouselSlide', () => {
 //   let slideProps;
@@ -99,18 +111,4 @@ describe('CarouselController', () => {
 //   });
 // });
 
-// describe('with the first slide selected', () => {
-//   it('wraps `slideIndex` to the max value when Prev is clicked', () => {
-//     wrapper.setState({ slideIndex: 0 });
-//     wrapper.find('[data-action="prev"]').simulate('click');
-//     expect(wrapper.state('slideIndex')).toBe(slides.length - 1);
-//   });
-// });
-// describe('with the last slide selected', () => {
-//   it('wraps `slideIndex` to the min vaue when Next is clicked', () => {
-//     wrapper.setState({ slideIndex: slides.length - 1 });
-//     wrapper.find('[data-action="next"]').simulate('click');
-//     expect(wrapper.state('slideIndex')).toBe(0);
-//   });
-// });
 // });
